f5ffd5485e78220762638ace28d5f7ad3d37ea95
soarflat <soarflat777@gmail.com>
2017-11-14 14:26:07 +0900

    [add] shopping cart demo

---
+++ src/js/shopping-cart/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container">
+    <nav-bar></nav-bar>
+    <!--ルートとマッチしたコンポーネントがここに表示される-->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import NavBar from './components/NavBar.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      NavBar
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
